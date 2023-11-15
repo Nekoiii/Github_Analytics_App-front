b50a266ab8d2c82e96082a1ef24688413b47b5f3
Nekoiii <10nekosa@gmail.com>
2023-11-16 08:54:39 +0900

    xx

--- app/src/components/GithubLoginButton.tsx
+++ app/src/components/GithubLoginButton.tsx
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from "react";
 
 export const GithubLoginButton = () => {
   const handleLogin = () => {
-    window.location.href = "http://localhost:3001/users/auth/github";
+    // Local:
+    // window.location.href = "http://localhost:3001/users/auth/github";
+    // Deploy:
+    window.location.href =
+      "https://github-analytics-app-api-edddea7fafa4.herokuapp.com/auth/github";
   };
 
   return <button onClick={handleLogin}>Login with GitHub</button>;
